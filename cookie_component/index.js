cff655fc12ba666d6897ae96a49a1c9734363a24
Dimple Mathew <36413448+dimplemathewkc@users.noreply.github.com>
2024-10-17 15:22:36 +0200

    ✨ New js code for get/set cookie

--- cookie_component/index.js
+++ cookie_component/index.js
@@ -1,8 +1,15 @@
 // cookie_component.js
 
-function getCookie(event) {
+function cookieManager(event) {
     data = event.detail;
-    name = data.args.spec;
+    const { name, action, value, days } = data.args.spec;
+    if (action === "get") {
+        getCookie(name);
+    } else if (action === "set") {
+        setCookie(name, value, days);
+    }
+}
+function getCookie(name) {
     console.log("Getting cookie:", name);
     const cookieArr = document.cookie.split(";");
     for (const cookiePair of cookieArr) {
@@ -15,5 +22,17 @@ function getCookie(event) {
     return null;
 }
 
-Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, getCookie)
+function setCookie(name, value, days) {
+    console.log("Setting cookie:", name, value, days);
+    let expires = "";
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+
+Streamlit.events.addEventListener(Streamlit.RENDER_EVENT, cookieManager)
 Streamlit.setComponentReady()
