580ce108f859bebd5aa766e465994f485ad0247f
Dimple Mathew <36413448+dimplemathewkc@users.noreply.github.com>
2024-10-17 15:21:46 +0200

    ✨ New component to get and set cookies

--- cookie_component/__init__.py
+++ cookie_component/__init__.py
@@ -1,12 +1,12 @@
 import streamlit.components.v1 as components
-_component_func = components.declare_component(f"cookie_comp", path="components",)
+_component_func = components.declare_component(f"cookie_comp", path="cookie_component",)
 
 
-def get_cookie(name):
+def cookie_manager(name, action, value=None, days=1):
     """
     This is a function to get the cookie, this is created using the streamlit component
     index.js is the javascript file that is used to get the cookie
     :name: name of the cookie
     """
-    value = _component_func(name=name, spec=name)
+    value = _component_func(spec={"name": name, "action": action, "value": value, "days": days})
     return value
